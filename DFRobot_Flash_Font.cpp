af614fa82bb0e29a8cfc90e6b36d4566621b79c2
ghjklzx <48232559+ghjklzx@users.noreply.github.com>
2020-02-24 19:47:27 +0800

    1

--- DFRobot_Flash_Font.cpp
+++ DFRobot_Flash_Font.cpp
@@ -1,277 +1,296 @@
-﻿#include <DFRobot_Flash_Font.h>
+﻿/*!
+ * @file DFRobot_Flash_Font.h
+ * @brief DFRobot_Flash_Font类的基础结构，基础方法的实现
+ * @copyright	Copyright (c) 2020 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @licence     The MIT License (MIT)
+ * @author [guo](xinyu.guo@dfrobot.com)
+ * @version  V1.0
+ * @date  2020-02-24
+ * @https:https://github.com/ghjklzx/DFRobot_Flash_Font
+ */
+#include <DFRobot_Flash_Font.h>
+#include <stdlib.h>
+DFRobot_Flash_Font::DFRobot_Flash_Font() 
+{
+}
 
-DFRobot_Flash_Font::DFRobot_Flash_Font() {}
+DFRobot_Flash_Font::~DFRobot_Flash_Font(void) 
+{
+  free (_utf8);
+}
 
 void DFRobot_Flash_Font::begin(void)
 {
-	pinMode(CS, OUTPUT); //设置SPI FLASH的片选引脚为输出模式
-	W25Q.read( HEAD_ADDRESS, &uniInfo , FONT_INFO_BYTES );//读取字模文件头部信息
+  pinMode(CS, OUTPUT); //设置SPI FLASH的片选引脚为输出模式
+  W25Q.read( HEAD_ADDRESS, &uniInfo , FONT_INFO_BYTES );//读取字模文件头部信息
+
 }
 
-void DFRobot_Flash_Font::cache(String &s)
+
+void DFRobot_Flash_Font::cache(char *s)
 {
-	_s = s;
-	_utf8 = (const uint8_t *)_s.c_str();
-	DBG(*_utf8);
-	DBG("_utf8[_index]=");DBG(_utf8[_index]);
-	_len = _s.length();
+  _len=strlen(s);
+  _utf8 = (uint8_t *)malloc(_len);
+  for(int i=0;i<_len;i++)
+  {
+    _utf8[i]= s[i];
+  };
+
 }
 
-uint32_t DFRobot_Flash_Font::utf8Get()
+void DFRobot_Flash_Font::readUni()
 {
-	DBG(_index);
-	DBG("_utf8[_index]=");DBG(_utf8[_index]);
-	uint32_t uni=0;
-	if ( _utf8[_index] == 0 || _utf8[_index] == '\n' )  /* '\n' terminates the string to support the string list procedures */
-	{
-		uni=0xff;
-		_index++;
-		DBG(_index);
-		DBG(uni);
-		return uni;
-		
-	} 
-	uint8_t utf8State = 0;
-	if(_utf8[_index] >= 0xfc)
-		{
-			utf8State = 5;
-			uni = _utf8[_index]&1;
-			_index++;
-			for(uint8_t i=1;i<=5;i++)
-			{
-				uni <<= 6;
-				uni |= (_utf8[_index]&0x3f);
-				utf8State--;
-				_index++;
-			}
-			DBG(_index);DBG(uni);
-			return uni;
-		}else if(_utf8[_index] >= 0xf8)
-		{
-			utf8State = 4;
-			uni = _utf8[_index]&3;
-			_index++;
-			for(uint8_t i=1;i<=4;i++)
-			{
-				uni <<= 6;
-				uni |= (_utf8[_index]& 0x03f);
-				utf8State--;
-				_index++;
-			}
-			DBG(_index);DBG(uni);
-			return uni;
-			
-		}else if(_utf8[_index] >= 0xf0)
-		{
-			utf8State = 3;
-			uni = _utf8[_index]&7;
-			_index++;
-			for(uint8_t i=1;i<=3;i++)
-			{
-				uni <<= 6;
-				uni |= (_utf8[_index]& 0x03f);
-				utf8State--;
-				_index++;
-				DBG(_index);
-			}
-			DBG(_index);DBG(uni);
-			return uni;
-			
-		}else if(_utf8[_index] >= 0xe0)
-		{
-			utf8State = 2;
-			DBG("index=");DBG(_index);
-			uni = _utf8[_index]&15;
-			_index++;
-			DBG("uni=");DBG(uni);
-			for(uint8_t i=1;i<=2;i++)
-			{
-				uni <<= 6;
-				uni |= (_utf8[_index]&0x03f);
-				utf8State--;
-				_index++;
-				DBG("uni=");DBG(uni);
-				DBG("index=");DBG(_index);
-			}
-			DBG(_index);DBG(uni);
-			return uni;	
-		}else if(_utf8[_index] >= 0xc0)
-		{
-			utf8State = 1;
-			uni = _utf8[_index]&0x1f;
-			_index++;
-			for(uint8_t i=1;i<=1;i++)
-			{
-				uni <<= 6;
-				uni |= (_utf8[_index]& 0x03f);
-				utf8State--;
-				_index++;
-				DBG(_index);
-			}
-			DBG(_index);DBG(uni);
-			return uni;
-		}else if(_utf8[_index] <=0x80)
-		{
-			uni |= (_utf8[_index]&0x7f);
-			_index++;
-			DBG("index")DBG(_index);
-			DBG("uni");DBG(uni);
-			return uni;
-		}
+  uint32_t uni=0;
+  if ( _utf8[_index] == 0 || _utf8[_index] == '\n' )  /* '\n' terminates the string to support the string list procedures */
+  {
+    uni=0xff;
+    _index++;
+    DBG(_index);
+    DBG(uni);
+    charData.ucode=uni;	
+  } 
+  uint8_t utf8State = 0;
+  if(_utf8[_index] >= 0xfc)
+    {
+      utf8State = 5;
+      uni = _utf8[_index]&1;
+      _index++;
+      for(uint8_t i=1;i<=5;i++)
+      {
+        uni <<= 6;
+        uni |= (_utf8[_index]&0x3f);
+        utf8State--;
+        _index++;
+      }
+      DBG(_index);DBG(uni);
+      charData.ucode=uni;
+    }else if(_utf8[_index] >= 0xf8)
+    {
+      utf8State = 4;
+      uni = _utf8[_index]&3;
+      _index++;
+      for(uint8_t i=1;i<=4;i++)
+      {
+        uni <<= 6;
+        uni |= (_utf8[_index]& 0x03f);
+        utf8State--;
+        _index++;
+      }
+      DBG(_index);DBG(uni);
+      charData.ucode=uni;
+      }else if(_utf8[_index] >= 0xf0)
+      {
+        utf8State = 3;
+        uni = _utf8[_index]&7;
+        _index++;
+      for(uint8_t i=1;i<=3;i++)
+      {
+        uni <<= 6;
+        uni |= (_utf8[_index]& 0x03f);
+        utf8State--;
+        _index++;
+        DBG(_index);
+      }
+      DBG(_index);DBG(uni);
+      charData.ucode=uni;
+	  }else if(_utf8[_index] >= 0xe0)
+	  {
+        utf8State = 2;
+        DBG("index=");DBG(_index);
+        uni = _utf8[_index]&15;
+        _index++;
+        DBG("uni=");DBG(uni);
+      for(uint8_t i=1;i<=2;i++)
+      {
+        uni <<= 6;
+        uni |= (_utf8[_index]&0x03f);
+        utf8State--;
+        _index++;
+        DBG("uni=");DBG(uni);
+        DBG("index=");DBG(_index);
+      }
+        DBG(_index);DBG(uni);
+        charData.ucode=uni;	
+      }else if(_utf8[_index] >= 0xc0)
+      {
+        utf8State = 1;
+        uni = _utf8[_index]&0x1f;
+        _index++;
+      for(uint8_t i=1;i<=1;i++)
+      {
+        uni <<= 6;
+        uni |= (_utf8[_index]& 0x03f);
+        utf8State--;
+        _index++;
+        DBG(_index);
+      }
+      DBG(_index);DBG(uni);
+      charData.ucode=uni;
+      }else if(_utf8[_index] <=0x80)
+      {
+        uni |= (_utf8[_index]&0x7f);
+        _index++;
+        DBG("index")DBG(_index);
+        DBG("uni");DBG(uni);
+        charData.ucode=uni;
+      }
 }
 
 bool DFRobot_Flash_Font::avaible()
 {
-	DBG(_len);
-	DBG(_index);
-	DBG("_utf8[_index]=");DBG(_utf8[_index]);
-	return !(_len ==_index);
+  DBG(_len);
+  DBG(_index);
+  DBG("_utf8[_index]=");DBG(_utf8[_index]);
+  return !(_len ==_index);
 }
 
-bool DFRobot_Flash_Font::getFont(uint32_t uni,uint8_t *buf, uint8_t& width, uint8_t& len,uint8_t& bytePerLine)
+bool DFRobot_Flash_Font::getFont()
 {
-	uint32_t address;
-	if (uni > (uniInfo.lastChar) || uni < (uniInfo.firstChar))
-	return false;
-	address = HEAD_ADDRESS + FONT_INFO_BYTES + uni * 6;
-	DBG();DBG(address);
-	charInfo_t charInfo;
-	W25Q.read(address, &charInfo, CHAR_ADDRESS_AND_BYTES_LEN);
-	DBG(charInfo.ptrCharData);
-	delay(10);
-	if (charInfo.ptrCharData == 0 || charInfo.len == 0)
-	return false;
-	charSpec_t charSpec;
-	address = (charInfo.ptrCharData) + HEAD_ADDRESS;
-	W25Q.read(address, &charSpec, CHAR_WIGTH_AND_BYTE_PER_LINE);
-	delay(10);
-	uint8_t charBufLen = (charInfo.len) - CHAR_WIGTH_AND_BYTE_PER_LINE;
-	address = address + CHAR_WIGTH_AND_BYTE_PER_LINE;
-	W25Q.read(address, buf, charBufLen);
-	
-	//判断是否成功赋值
-	DBG(charSpec.width);
-	width = charSpec.width;
-	len = charBufLen;
-	bytePerLine = charSpec.bytePerLine;
-	DBG("width=");DBG(width);
-	DBG("len=");DBG(len);
-	DBG("bytePerLine");DBG(bytePerLine);
-	return true;
-		
-	
+  uint32_t address;
+  if (charData.ucode > (uniInfo.lastChar) || charData.ucode < (uniInfo.firstChar))
+  return false;
+  address = HEAD_ADDRESS + FONT_INFO_BYTES + charData.ucode * 6;
+  DBG();DBG(address);
+  charInfo_t charInfo;
+  W25Q.read(address, &charInfo, CHAR_ADDRESS_AND_BYTES_LEN);
+  DBG(charInfo.ptrCharData);
+  delay(10);
+  if (charInfo.ptrCharData == 0 || charInfo.len == 0)
+  return false;
+  charSpec_t charSpec;
+  address = (charInfo.ptrCharData) + HEAD_ADDRESS;
+  W25Q.read(address, &charSpec, CHAR_WIGTH_AND_BYTE_PER_LINE);
+  delay(10);
+  uint8_t charBufLen = (charInfo.len) - CHAR_WIGTH_AND_BYTE_PER_LINE;
+  address = address + CHAR_WIGTH_AND_BYTE_PER_LINE;
+  W25Q.read(address, &charData.charBufer, charBufLen);
+  //判断是否成功赋值
+  DBG(charSpec.width);
+  charData.width = charSpec.width;
+  charData.lenth = charBufLen;
+  charData.bytePerLine = charSpec.bytePerLine;
+  return true;
 }
 
-void DFRobot_Flash_Font::drawStringMap(uint8_t *charBuf,uint8_t width, uint8_t len,uint8_t bytePerLine ) 
+void DFRobot_Flash_Font::drawStringMap() 
 {
-	DBG(charBuf[0]);
-	DBG(width);
-	DBG(len);
-	DBG(bytePerLine);
-	int count = 0;
-	int w = 0;
-	for (int i = 0; i < len; i++) {
-		uint8_t mask = charBuf[i];
-		int n = 8;
-		if ((w + 8) > width)
-			n =width - w;
-		for (int p = 0; p < n; p++) {
-			if (mask & 0x80) SerialUSB.print("■");
-			else SerialUSB.print("□");
-			mask <<= 1;
-		}
-		w += 8;
-		count++;
-		if (count == bytePerLine) {
-			count = 0;
-			w = 0;
-			SerialUSB.println();
-		}
-	}
-	SerialUSB.println();
+  int count = 0;
+  int w = 0;
+  for (int i = 0; i < charData.lenth; i++) 
+  {
+    uint8_t mask = charData.charBufer[i];
+    int n = 8;
+    if ((w + 8) > charData.width)
+    n =charData.width - w;
+    for (int p = 0; p < n; p++) 
+    {
+      if (mask & 0x80) SerialUSB.print("■");
+      else
+        SerialUSB.print("□");
+        mask <<= 1;
+    }
+      w += 8;
+      count++;
+      if (count == charData.bytePerLine)
+      {
+        count = 0;
+        w = 0;
+        SerialUSB.println();
+      }
+  }
+  SerialUSB.println();
 	
 }
 
 void DFRobot_Flash_Font::eraseSpace(void) 
-{	uint32_t address=HEAD_ADDRESS;
- 	pinMode(CS, OUTPUT);
-	SerialUSB.println("Preparing font space in flash for font library");
-	SerialUSB.println("This will erase the font space memory form flash, plaese wait...\n");
-	SerialUSB.println("-----------------------Erase Space-------------------------");
-	SerialUSB.print("Erasing | ");
+{
+  uint32_t address=HEAD_ADDRESS;
+  pinMode(CS, OUTPUT);
+  SerialUSB.println("Preparing font space in flash for font library");
+  SerialUSB.println("This will erase the font space memory form flash, plaese wait...\n");
+  SerialUSB.println("-----------------------Erase Space-------------------------");
+  SerialUSB.print("Erasing | ");
+  for (int count = 0; count < 512; count++)
+  {
+    W25Q.eraseSector(address);
+    if (count != 0 && count % 11 == 0)
+      SerialUSB.print("▋");
+      address += 4096;
+  }
 	
-	for (int count = 0; count < 512; count++) {
-		W25Q.eraseSector(address);
-		if (count != 0 && count % 11 == 0)
-			SerialUSB.print("▋");
-		address += 4096;
-	}
-	
-	SerialUSB.println(" | 100%");
-	SerialUSB.println("-----------------------------------------------------------\n");
-	SerialUSB.println("font space prepared. ");
+  SerialUSB.println(" | 100%");
+  SerialUSB.println("-----------------------------------------------------------\n");
+  SerialUSB.println("font space prepared. ");
 }
 
 void DFRobot_Flash_Font::SDInit(void) 
 {
-	pinMode(CS, OUTPUT);
-	SerialUSB.print("Initializing SD card...");
-	if (!SD.begin(CS)) {
-		SerialUSB.println("initialization failed!");
-		while (1);
-	}
-	SerialUSB.println("initialization done.");
-	fileData = SD.open("Noto.xbf");
-
-	if (fileData) {
-		fileSize = fileData.size();
-		SerialUSB.print("Ready to write to flash, total size: ");
-		SerialUSB.println(fileSize);
-	} else {
-		SerialUSB.println("error opening Noto.xbf, please check connection or file...");
-		while (1);
-	}
-	SerialUSB.println();
+  pinMode(CS, OUTPUT);
+  SerialUSB.print("Initializing SD card...");
+  if (!SD.begin(CS))
+  {
+    SerialUSB.println("initialization failed!");
+    while (1);
+  }
+  SerialUSB.println("initialization done.");
+  fileData = SD.open("Noto.xbf");
+  if (fileData) 
+  {
+    fileSize = fileData.size();
+    SerialUSB.print("Ready to write to flash, total size: ");
+    SerialUSB.println(fileSize);
+  } 
+  else 
+  {
+    SerialUSB.println("error opening Noto.xbf, please check connection or file...");
+    while (1);
+  }
+  SerialUSB.println();
 }
 
 void DFRobot_Flash_Font::burnFontLib(uint32_t address,uint32_t MAXBUFSIZE) 
 {
-	uint8_t buf[MAXBUFSIZE];
-	int count = 0;
-	uint32_t rest = fileSize;
-	uint16_t len;
-	
-	pinMode(CS, OUTPUT);
-	SerialUSB.println("--------------------------Burn Font----------------------------");
-	SerialUSB.print("Burning | ");
-	
-	while(rest) {
-		if (rest > MAXBUFSIZE)
-			len = MAXBUFSIZE;
-		else
-			len = rest;
-			
-		fileData.read(buf, len);
-		W25Q.write(address, buf, len);
-		
-		rest -= len;
-		address += len;
-		if (fileData.position() == (fileSize - rest)) {
-			if ((fileSize - rest) >= (count * fileSize / 50)) {
-				SerialUSB.print("▋");
-				count++;
-			}
-		} else {break;}
-	}
-	if (fileData.position() == fileSize) {
-		SerialUSB.println(" | 100%");
-		SerialUSB.println("---------------------------------------------------------------");
-		
-		SerialUSB.println("\nFinish transfer. Now you can use the font Library form board.");
-	} else {
-		SerialUSB.println(" | failed");
-		SerialUSB.println("---------------------------------------------------------------");
-		SerialUSB.println("\nTransfer failed. please reset your board and transfer one more time!");
-	}
-	fileData.close();
+  uint8_t buf[MAXBUFSIZE];
+  int count = 0;
+  uint32_t rest = fileSize;
+  uint16_t len;
+  pinMode(CS, OUTPUT);
+  SerialUSB.println("--------------------------Burn Font----------------------------");
+  SerialUSB.print("Burning | ");
+  while(rest) 
+  {
+    if (rest > MAXBUFSIZE)
+    len = MAXBUFSIZE;
+    else
+    len = rest;
+    fileData.read(buf, len);
+    W25Q.write(address, buf, len);
+    rest -= len;
+    address += len;
+    if (fileData.position() == (fileSize - rest)) 
+	{
+      if ((fileSize - rest) >= (count * fileSize / 50)) 
+	  {
+        SerialUSB.print("▋");
+        count++;
+      }
+    }
+	else  
+    {
+      break;
+    }
+  }
+  if (fileData.position() == fileSize)
+  {
+    SerialUSB.println(" | 100%");
+    SerialUSB.println("---------------------------------------------------------------");
+    SerialUSB.println("\nFinish transfer. Now you can use the font Library form board.");
+  } 
+  else 
+  {
+    SerialUSB.println(" | failed");
+    SerialUSB.println("---------------------------------------------------------------");
+    SerialUSB.println("\nTransfer failed. please reset your board and transfer one more time!");
+  }
+  fileData.close();
 }
--- DFRobot_Flash_Font.h
+++ DFRobot_Flash_Font.h
@@ -1,10 +1,19 @@
+/*!
+ * @file DFRobot_Flash_Font.h
+ * @brief Flash内置字体库
+ * @n 这是一个Flash内置字体使用库
+ * @n 可调用Flash内置的字体，显示中英日韩文字
+ * @copyright	Copyright (c) 2020 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @licence     The MIT License (MIT)
+ * @author [guo](xinyu.guo@dfrobot.com)
+ * @version  V1.0
+ * @date  2020-02-24
+ * @https://github.com/ghjklzx/M0_Font
+ */
 #ifndef __DFRobot_Flash_Font_H
 #define __DFRobot_Flash_Font_H
 
 #include "Arduino.h"
-#include <SPI.h>
-#include <UD.h>
-#include <W25QSPIFlash.h>
 
 #define ENABLE_DBG
 #ifdef ENABLE_DBG
@@ -16,58 +25,118 @@
 #define HEAD_ADDRESS 0xc00000//SPIflash首地址
 
 #define CS 32//SPIflash片选引脚
-#define CS_H  digitalWrite(CS, HIGH)
-#define CS_L  digitalWrite(CS, LOW)
-
-#define FONT_INFO_BYTES                18//单个字模大小
-#define CHAR_ADDRESS_AND_BYTES_LEN     6//
-#define CHAR_WIGTH_AND_BYTE_PER_LINE   4//
+#define FONT_INFO_BYTES                18//字模信息数据长度
+#define CHAR_ADDRESS_AND_BYTES_LEN     6//字模信息数据长度
+#define CHAR_WIGTH_AND_BYTE_PER_LINE   4//字模信息数据长度
 #endif
 
 class DFRobot_Flash_Font
 {
 public:
-    typedef struct 
-    {
-	    char title[4];//juix
-	    uint16_t height;//16
-	    uint16_t width;//16
-	    uint16_t baseline;//12
-	    uint16_t xHeight;
-	    uint16_t yHeight;
-	    uint16_t firstChar;//0
-	    uint16_t lastChar; //65535
+  typedef struct {
+    char title[4];//
+    uint16_t height;//字体高度
+    uint16_t width;//字体宽度
+    uint16_t baseline;//数据长度
+    uint16_t xHeight;//字模x轴
+    uint16_t yHeight;//字模y轴
+    uint16_t firstChar;//0
+    uint16_t lastChar; //65535
     } __attribute__ ((packed)) uniInfo_t;//所有字符的信息
 
-    typedef struct {
-	    uint32_t ptrCharData;
-	    uint16_t len;
+  typedef struct {
+    uint32_t ptrCharData;
+    uint16_t len;//字模数据长度
     } __attribute__ ((packed)) charInfo_t;//字符信息
 
-    typedef struct {
-	    uint16_t width;
-	    uint16_t bytePerLine;
+  typedef struct {
+    uint16_t width;
+    uint16_t bytePerLine;
     } __attribute__ ((packed)) charSpec_t;//字符信息
-	uniInfo_t uniInfo;
-	uint32_t transSize = 0;
-	uint32_t fileSize;
-	File fileData;
+	
+  typedef struct {
+    uint32_t ucode;
+    uint8_t charBufer[32];
+    uint8_t width;
+    uint8_t lenth;
+    uint8_t bytePerLine;
+    } charData_t; //用户使用的字模信息
+
+  charData_t charData; //提供给用户字模数据
+  uniInfo_t uniInfo;//字模的info信息
+  uint32_t transSize = 0;
+  uint32_t fileSize;
+  File fileData;
 
 public:
-	DFRobot_Flash_Font();
-	void begin(void);
-	void cache(String &s);
-	bool avaible(void);
-	uint32_t utf8Get();
-	uint16_t transfer(uint8_t utf8,uint16_t& index);
-	bool getFont(uint32_t uni,uint8_t *buf, uint8_t& width, uint8_t& len,uint8_t& bytePerLine);
-	void drawStringMap(uint8_t *charBuf,uint8_t width, uint8_t len,uint8_t bytePerLine);
-	void eraseSpace(void);
-	void burnFontLib(uint32_t fontAddress,uint32_t MAXBUFSIZE);
-	void SDInit(void);
-private:
-	String _s;
-	const uint8_t *_utf8;
-	uint16_t _index=0;
-	uint8_t _len=0;
+  /**
+   * @brief 构造函数
+   * @param 
+   */
+  DFRobot_Flash_Font();
+  
+  /**
+   * @brief 析构函数
+   * @param 释放_utf8字符数据
+   */
+  ~DFRobot_Flash_Font();
+
+  /**
+   * @brief 初始化
+   * @param 设置flash，并读取字模头部信息
+   */
+  void begin(void);
+
+  /**
+   * @brief 缓存字符信息
+   * @param 
+   */
+  void cache(char *s);
+
+  /**
+   * @brief 判断字模是否输出完成
+   * @param  
+   */
+  bool avaible(void);
+  
+  /**
+   * @brief 读取unicode
+   * @param 
+   */
+  void readUni();
+  
+  /**
+   * @brief 获得字模信息
+   * @param 
+   */
+  bool getFont();
+
+  /**
+   * @brief 在打印窗口打印字模
+   * @param 
+   */
+  void drawStringMap();
+
+  /**
+   * @brief 擦除字模空间
+   * @param 
+   */
+  void eraseSpace(void);
+
+  /**
+   * @brief 写入字模
+   * @param 
+   */
+  void burnFontLib(uint32_t fontAddress,uint32_t MAXBUFSIZE);
+
+  /**
+   * @brief 初始化U盘
+   * @param 
+   */
+  void SDInit(void);
+  
+private: 
+  uint8_t *_utf8;
+  uint16_t _index=0;
+  uint8_t _len=0;
 };
